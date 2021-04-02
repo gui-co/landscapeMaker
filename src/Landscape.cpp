bfe699ed5a66bf9cec46e1eebcfde3a666f1293f
Guillaume Communie <gui-co@posteo.net>
2021-04-02 12:11:06 +0200

    Landscape image from distance data

--- src/Landscape.cpp
+++ src/Landscape.cpp
@@ -6,11 +6,19 @@
 #include <QFile>
 #include <QTextStream>
 
+#include <limits>
+#include <cmath>
+
 Landscape::Landscape(int width, int height)
 	: landscapeWidth(width), landscapeHeight(height)
 {
 	landscapeDistances.resize(landscapeWidth * landscapeHeight);
 	landscapeDistances.fill(0.0);
+	for (size_t i = 0; i < landscapeDistances.size(); i++)
+		if (i / landscapeWidth < 512)
+			landscapeDistances[i] = std::numeric_limits<double>::infinity();
+		else
+			landscapeDistances[i] = rand() % 300;
 }
 
 Landscape::~Landscape(void)
@@ -25,21 +33,28 @@ void Landscape::save(const QString &name, const QDir &saveDirectory)
 	QString imagePath = currentLandscapeDir.filePath("image.png");
 	QString iniPath = currentLandscapeDir.filePath("landscape.ini");
 
+	// distance max
+	double distanceMax = 0;
+	for (double distance : landscapeDistances)
+		if ((!std::isinf(distance)) && (distance > distanceMax))
+			distanceMax = distance;
+
 	// image
-	QImage image(2048, 1024, QImage::Format_ARGB32);
-	for (int i = 0 ; i < 2048 ; i++)
+	QImage image(landscapeWidth, landscapeHeight, QImage::Format_ARGB32);
+	QColor pixelColor;
+	float h, s, v, a;
+	for (size_t i = 0; i < landscapeDistances.size() ; i++)
 	{
-		int j = 0;
-		while (j < 512)
-		{
-			image.setPixelColor(i, j, QColor(0, 0, 0, 0));
-			j++;
-		}
-		while (j < 1024)
-		{
-			image.setPixelColor(i, j, QColor(255, 255, 255, 255));
-			j++;
-		}
+		h = 0.0;
+		s = 1.0;
+		v = 1.0;
+		a = 1.0;
+		if (std::isinf(landscapeDistances[i]))
+			a = 0.0;
+		else
+			h = landscapeDistances[i] / distanceMax;
+		pixelColor.setHsvF(h, s, v, a);
+		image.setPixelColor(i % landscapeWidth, i / landscapeWidth, pixelColor);
 	}
 	image.save(imagePath);
 
