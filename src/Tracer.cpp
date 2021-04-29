3f89c10d8318bd1e74a479ed38fd722503288bb9
Guillaume Communie <gui-co@posteo.net>
2021-04-29 22:50:37 +0200

    Coordinate conversion in tracer

--- src/Tracer.cpp
+++ src/Tracer.cpp
@@ -8,20 +8,15 @@ const double Tracer::WGS84_EXCENTRICITY_SQUARED = 0.00669437999014;
 
 Tracer::Tracer(double observerLatitude, double observerLongitude,
                double observerElevation)
-	: tile(nullptr)
+	: latitude(observerLatitude), longitude(observerLongitude),
+	  elevation(observerElevation), tile(nullptr)
 {
-	double n = WGS84_SEMI_MAJOR_AXIS / (sqrt(1 - WGS84_EXCENTRICITY_SQUARED
-	           * pow(sin(observerLatitude), 2)));
-	observerX = (n + observerElevation) * cos(observerLatitude)
-	            * cos(observerLongitude);
-	observerY = (n + observerElevation) * cos(observerLatitude)
-	            * sin(observerLongitude);
-	observerZ = (pow(WGS84_SEMI_MINOR_AXIS, 2) / pow(WGS84_SEMI_MAJOR_AXIS, 2)
-	            * n + observerElevation) * sin(observerLatitude);
+	ecefObserver = geodeticToEcef(observerLatitude, observerLongitude,
+	                              observerElevation);
 
-	int tileCornerLat = static_cast<int>(observerLatitude);
+	int tileCornerLat = static_cast<int>(latitude);
 	tileCornerLat = tileCornerLat - tileCornerLat % 5;
-	int tileCornerLon = static_cast<int>(observerLongitude);
+	int tileCornerLon = static_cast<int>(longitude);
 	tileCornerLon = tileCornerLon - tileCornerLon % 5;
 	tile = new Tile(tileCornerLat, tileCornerLon, *this);
 }
@@ -29,5 +24,38 @@ Tracer::Tracer(double observerLatitude, double observerLongitude,
 Tracer::~Tracer(void)
 {
 	delete tile;
+}
+
+Vector3d Tracer::geodeticToEcef(double latitude, double longitude,
+                                double elevation) const
+{
+	double n = WGS84_SEMI_MAJOR_AXIS
+	           / sqrt(1 - WGS84_EXCENTRICITY_SQUARED * pow(sin(latitude), 2));
+	double x = (n + elevation) * cos(latitude) * cos(longitude);
+	double y = (n + elevation) * cos(latitude) * sin(longitude);
+	double z = ((pow(WGS84_SEMI_MINOR_AXIS, 2) / pow(WGS84_SEMI_MAJOR_AXIS, 2))
+	           * n + elevation) * sin(latitude);
+
+	return Vector3d(x, y, z);
+}
+
+Vector3d Tracer::ecefToEnu(const Vector3d &ecef) const
+{
+	Vector3d diff = ecef - ecefObserver;
+	double x = -sin(longitude) * diff.getX() + cos(longitude) * diff.getY();
+	double y = -sin(latitude) * cos(longitude) * diff.getX()
+	           - sin(latitude) * sin(longitude) * diff.getY()
+	           + cos(latitude) * diff.getZ();
+	double z = cos(latitude) * cos(longitude) * diff.getX()
+	           + cos(latitude) * sin(longitude) * diff.getY()
+	           + sin(longitude) * diff.getZ();
+
+	return Vector3d(x, y, z);
+}
+
+Vector3d Tracer::geodeticToEnu(double latitude, double longitude,
+                               double elevation) const
+{
+	return ecefToEnu(geodeticToEcef(latitude, longitude, elevation));
 }
 
--- src/Tracer.hpp
+++ src/Tracer.hpp
@@ -2,21 +2,27 @@
 #define TRACER_HPP
 
 #include "Tile.hpp"
+#include "Vector3d.hpp"
 
 class Tracer
 {
 public:
 	Tracer(double, double, double);
 	~Tracer(void);
+	Vector3d geodeticToEcef(double latitude, double longitude,
+	                        double elevation) const;
+	Vector3d ecefToEnu(const Vector3d &ecef) const;
+	Vector3d geodeticToEnu(double latitude, double longitude, double elevation) const;
 	void traceLandscape(void);
 	static const double WGS84_SEMI_MAJOR_AXIS;
 	static const double WGS84_SEMI_MINOR_AXIS;
 	static const double WGS84_EXCENTRICITY_SQUARED;
 
 private:
-	double observerX;
-	double observerY;
-	double observerZ;
+	double latitude;
+	double longitude;
+	double elevation;
+	Vector3d ecefObserver;
 	Tile *tile;
 };
 
