28420d3ebff799402aa0d304bbb0b445a0ef5e85
Guillaume Communie <gui-co@posteo.net>
2021-07-20 20:16:26 +0200

    Formulas need radian values

--- src/Tracer.cpp
+++ src/Tracer.cpp
@@ -29,26 +29,30 @@ Tracer::~Tracer(void)
 Vector3d Tracer::geodeticToEcef(double latitude, double longitude,
                                 double elevation) const
 {
+	double latRad = latitude * M_PI / 180.0;
+	double lonRad = longitude * M_PI / 180.0;
 	double n = WGS84_SEMI_MAJOR_AXIS
-	           / sqrt(1 - WGS84_EXCENTRICITY_SQUARED * pow(sin(latitude), 2));
-	double x = (n + elevation) * cos(latitude) * cos(longitude);
-	double y = (n + elevation) * cos(latitude) * sin(longitude);
+	           / sqrt(1 - WGS84_EXCENTRICITY_SQUARED * pow(sin(latRad), 2));
+	double x = (n + elevation) * cos(latRad) * cos(lonRad);
+	double y = (n + elevation) * cos(latRad) * sin(lonRad);
 	double z = ((pow(WGS84_SEMI_MINOR_AXIS, 2) / pow(WGS84_SEMI_MAJOR_AXIS, 2))
-	           * n + elevation) * sin(latitude);
+	           * n + elevation) * sin(latRad);
 
 	return Vector3d(x, y, z);
 }
 
 Vector3d Tracer::ecefToEnu(const Vector3d &ecef) const
 {
+	double latRad = latitude * M_PI / 180.0;
+	double lonRad = longitude * M_PI / 180.0;
 	Vector3d diff = ecef - ecefObserver;
-	double x = -sin(longitude) * diff.getX() + cos(longitude) * diff.getY();
-	double y = -sin(latitude) * cos(longitude) * diff.getX()
-	           - sin(latitude) * sin(longitude) * diff.getY()
-	           + cos(latitude) * diff.getZ();
-	double z = cos(latitude) * cos(longitude) * diff.getX()
-	           + cos(latitude) * sin(longitude) * diff.getY()
-	           + sin(longitude) * diff.getZ();
+	double x = -sin(latRad) * diff.getX() + cos(lonRad) * diff.getY();
+	double y = -sin(latRad) * cos(lonRad) * diff.getX()
+	           - sin(latRad) * sin(lonRad) * diff.getY()
+	           + cos(latRad) * diff.getZ();
+	double z = cos(latRad) * cos(lonRad) * diff.getX()
+	           + cos(latRad) * sin(lonRad) * diff.getY()
+	           + sin(latRad) * diff.getZ();
 
 	return Vector3d(x, y, z);
 }
